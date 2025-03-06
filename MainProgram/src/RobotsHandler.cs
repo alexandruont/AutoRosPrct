7d98aca89dd4d3568939cb679b76af686ddcea58
alexandruont <alex.ont@icloud.com>
2025-03-06 14:00:52 +0200

    Stop function

--- MainProgram/src/RobotsHandler.cs
+++ MainProgram/src/RobotsHandler.cs
@@ -32,5 +32,11 @@ namespace TCPIPServer
                 _robotControllers[ipAddress] = new RobotController(client);
             }
         }
+
+        private void Stop()
+        {
+            _listener.Stop();
+            _robotControllers.Clear();
+        }
     }
 }
